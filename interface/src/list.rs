e5a2a5d4b05da4752d5435d7c5bdce6444b79e4a
m3y <masozawa@pay2.jp>
2020-12-24 11:29:17 +0900

    update interface

--- interface/src/list.rs
+++ interface/src/list.rs
@@ -1,10 +1,7 @@
-pub trait List<T>
-where
-    T: std::fmt::Debug,
-{
+pub trait List<T: Clone> {
     fn size(&self) -> usize;
     fn get(&self, i: usize) -> Option<T>;
-    fn set(&self, i: usize, x: T);
-    fn add(&self, i: usize, x: T);
-    fn remove(&self, i: usize);
+    fn set(&mut self, i: usize, x: T) -> Option<T>;
+    fn add(&mut self, i: usize, x: T);
+    fn remove(&mut self, i: usize);
 }
